acb105614fd0f493d453191c57c0f22e569c6030
Jimmy06 <Jimmy062006@users.noreply.github.com>
2021-10-09 17:50:42 +0100

    Add project files.

---
+++ RTMLauncherServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RTMLauncherService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Just4DNS")]
+[assembly: AssemblyProduct("RTMLauncherService")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("527a5e05-7e49-4648-92c4-102203439325")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.6")]
+[assembly: AssemblyFileVersion("0.0.0.6")]
---
+++ RTMLauncherServer/RTMLauncher.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace RTMLauncherService
+{
+    public partial class RTMLauncher : ServiceBase
+    {
+        Process process;
+        bool loaded = false;
+
+        public RTMLauncher()
+        {
+            InitializeComponent();
+            var timer = new System.Timers.Timer(10000);
+            timer.Elapsed += new ElapsedEventHandler(Check_Running);
+            timer.Enabled = true;
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("RTMLauncherService"))
+            {
+                EventLog.CreateEventSource(
+                    "RTMLauncherService", "RTMLauncher");
+            }
+            eventLog1.Source = "RTMLauncherService";
+            eventLog1.Log = "RTMLauncher";
+        }
+
+        private async void Check_Running(object sender, ElapsedEventArgs e)
+        {
+            if (loaded && process.HasExited)
+            {
+                await StartMiner().ConfigureAwait(false);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Waiting for debugger to attach");
+
+#if DEBUG
+            while (!Debugger.IsAttached)
+            {
+                Thread.Sleep(100);
+            }
+#endif
+
+            Console.WriteLine("Debugger attached");
+            eventLog1.WriteEntry("Starting RTMLauncher.");
+            StartMiner().ConfigureAwait(false);
+        }
+
+        private async Task<bool> StartMiner()
+        {
+            try
+            {
+                process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        WorkingDirectory = $"{Properties.Settings.Default.MinerPath}",
+                        FileName = $"{Properties.Settings.Default.MinerPath}\\{Properties.Settings.Default.MinerExe}",
+                        Arguments = $"-c {Properties.Settings.Default.MinerConfig}",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
+                    }
+                };
+
+                //* Set your output and error (asynchronous) handlers
+                process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+                process.Exited += new EventHandler(ProcessQuit);
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                loaded = true;
+            }
+
+            catch (Exception ex)
+            {
+                eventLog1.WriteEntry($"{ex}", EventLogEntryType.Error);
+            }
+
+            await Task.CompletedTask;
+            return true;
+        }
+
+        private async void ProcessQuit(object sender, EventArgs e)
+        {
+            await StartMiner().ConfigureAwait(false);
+        }
+
+        private void OutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine($"{e.Data}");
+                File.AppendAllText($"{Properties.Settings.Default.MinerPath}\\console.log", $"{e.Data}{Environment.NewLine}");
+            }
+            catch { }
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stopping RTMLauncher.");
+            try
+            {
+                process.Kill();
+            }
+            catch { }
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("Resuming RTMLauncher.");
+        }
+
+        private void EventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+
+        }
+
+
+    }
+}
